4deee3cd7472cea66c7d1577cd7ced3c8770f7ea
Sandra Blanco <sandra.blanco18@gmail.com>
2026-06-22 17:10:17 +0200

    key for looking for path

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from module import suml, prodl
+#from module import suml, prodl
+#zeroes = [ 0 for i in range(5)]
+#ones = [ 1 for i in range(5)]
+#print(suml(zeroes) == 0)
+#print(prodl(zeroes) == 0)
+#print(prodl(ones))
+ #print(module.counter)
+
+from sys import path 
+path.append('..\\modules')
+import module
 zeroes = [ 0 for i in range(5)]
 ones = [ 1 for i in range(5)]
-print(suml(zeroes) == 0)
-print(prodl(zeroes) == 0)
- #print(module.counter)
+print(module.suml(zeroes))
+print(module.prodl(ones))
